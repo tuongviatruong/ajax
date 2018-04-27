7f2a6521503df23b4184ea7b2ec546730c56d11e
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-27 12:11:08 -0700

    Finished part 1 - fortune

---
+++ static/ajax-exercise.js
@@ -0,0 +1,49 @@
+"use strict";
+
+
+// PART 1: SHOW A FORTUNE
+
+function showFortune(results) {
+
+    // TODO: get the fortune and show it in the #fortune-text div
+    $('#fortune-text').html(results);
+	
+}
+function getFortune(){
+	$.get('/fortune', showFortune)
+}
+
+$('#get-fortune-button').on('click', getFortune);
+
+
+
+
+// PART 2: SHOW WEATHER
+
+function showWeather(evt) {
+    evt.preventDefault();
+
+    let url = "/weather.json";
+    let formData = {"zipcode": $("#zipcode-field").val()};
+
+
+    // TODO: request weather with that URL and show the forecast in #weather-info
+}
+
+$("#weather-form").on('submit', showWeather);
+
+
+
+
+// PART 3: ORDER MELONS
+
+function orderMelons(evt) {
+    evt.preventDefault();
+
+    // TODO: show the result message after your form
+    // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+}
+
+$("#order-form").on('submit', orderMelons);
+
+
